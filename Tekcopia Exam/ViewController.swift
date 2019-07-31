5a21a4a6614330e83b9f036591ae3f7949ceada9
Allan Pagdanganan <pagdanganan.allan@gmail.com>
2019-07-31 12:08:01 +0800

    Initial Commit

---
+++ Tekcopia Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tekcopia Exam
+//
+//  Created by Allan Pagdanganan on 31/07/2019.
+//  Copyright © 2019 Allan Pagdanganan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
